8a5da9a5b9cbb78912f5516430e15d159b8f2975
Chase HMTV <ssgear456@gmail.com>
2026-07-29 14:59:16 -0400

    Minor adjustments and adding ReadMe skeleton

--- src/ErrorLoading.jsx
+++ src/ErrorLoading.jsx
@@ -3,7 +3,7 @@ function ErrorLoading({ error, loading }) {
     return (
         <>
             {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
-            {loading && <p style={{ textAlign: 'center', color: '#274b7e' }}>Getting your weather</p>}
+            {loading && <p style={{ textAlign: 'center', color: '#196130', fontStyle: 'italic' }}>Getting your weather...</p>}
         </>
     );
 }
